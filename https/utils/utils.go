f8c03cfd1f5c7abfa2d2f5c5f2386dc9f84cf16f
BuffOvrFlw <ffrick0001980@proton.me>
2026-02-12 01:43:03 +0300

    More JWT security + Secret generation

--- https/utils/utils.go
+++ https/utils/utils.go
@@ -1,17 +1,39 @@
 package https_utils
 
 import (
-    "time"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"time"
 
-    "github.com/IzomSoftware/GinWrapper/common/configuration"
-    "github.com/golang-jwt/jwt/v5"
+	"github.com/IzomSoftware/GinWrapper/common/configuration"
+	"github.com/golang-jwt/jwt/v5"
 )
-
-var secret = []byte(configuration.ConfigHolder.Tokenizer.TokenizerSecret)
 
 type User struct {
     Username string `json:"username"`
     jwt.RegisteredClaims
+}
+
+func GenerateRandomSecret(size int) (string, error) {
+    bytes := make([]byte, size)
+
+    _, err := rand.Read(bytes)
+    if err != nil {
+        return "", err
+    }
+	
+    return base64.URLEncoding.EncodeToString(bytes), nil
+}
+
+func getSecret() ([]byte, error) {
+    secret := configuration.ConfigHolder.Protections.Tokenizer.TokenizerSecret
+
+    if secret == "" {
+        return nil, fmt.Errorf("Empty Secret")
+    }
+
+    return base64.URLEncoding.DecodeString(secret)
 }
 
 func GenerateToken(id string, exp time.Duration) (string, error) {
@@ -25,11 +47,26 @@ func GenerateToken(id string, exp time.Duration) (string, error) {
     }
 
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	secret, err := getSecret()
+    if err != nil {
+        return "", err
+    }
+
     return token.SignedString(secret)
 }
 
 func ParseToken(tokenString string) (string, error) {
+	secret, err := getSecret()
+
+	if err != nil {
+		return "", err
+	}
+
     token, err := jwt.ParseWithClaims(tokenString, &User{}, func(token *jwt.Token) (interface{}, error) {
+        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, nil
+        }
         return secret, nil
     })
 
