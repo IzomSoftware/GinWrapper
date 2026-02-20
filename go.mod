6ab8f7786e948ab49c5ab4dd1b597de0ca0fd9d6
realstresser <rezaebr3234s4@gmail.com>
2026-02-20 21:33:48 +0300

    Redis implementation (both embedded and server)

--- go.mod
+++ go.mod
@@ -4,13 +4,19 @@ go 1.24
 
 require (
 	github.com/BurntSushi/toml v1.5.0
+	github.com/alicebob/miniredis/v2 v2.36.1
 	github.com/gin-gonic/gin v1.10.1
+	github.com/redis/go-redis/v9 v9.18.0
 	github.com/sirupsen/logrus v1.9.3
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/yuin/gopher-lua v1.1.1 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	modernc.org/libc v1.37.6 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
 	modernc.org/memory v1.7.2 // indirect
--- redis/redis.go
+++ redis/redis.go
@@ -1,1 +1,100 @@
 package redis
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"time"
+
+	"github.com/IzomSoftware/GinWrapper/configuration"
+	"github.com/alicebob/miniredis/v2"
+	"github.com/redis/go-redis/v9"
+)
+
+var redisCl *redis.Client
+var redisCtx context.Context
+
+/*
+ * MiniRedis
+ */
+func InitEmbeddedRedis() (*miniredis.Miniredis, error) {
+	miniRedis, err := miniredis.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	redisCl = redis.NewClient(&redis.Options{
+		Addr: miniRedis.Addr(),
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+	redisCtx = ctx
+
+	return miniRedis, nil
+}
+
+func ShutdownEmbeddedRedis(miniRedis *miniredis.Miniredis) {
+	miniRedis.Close()
+}
+
+/*
+ * Redis
+ */
+func InitRedis(config configuration.RedisConfiguration) error {
+	redisOpts := &redis.Options{
+		Addr:         fmt.Sprintf("%s:%x", config.Hostname, config.Port),
+		Username:     config.Username,
+		Password:     config.Password,
+		DB:           config.Database,
+		PoolSize:     config.PoolSize,
+		MinIdleConns: config.MinIdleConnections,
+		MaxRetries:   config.MaxRetries,
+		PoolTimeout:  time.Duration(config.PoolTimeout) * time.Second,
+		DialTimeout:  time.Duration(config.DialTimeout) * time.Second,
+		ReadTimeout:  time.Duration(config.ReadTimeout) * time.Second,
+		WriteTimeout: time.Duration(config.WriteTimeoutSec) * time.Second,
+		TLSConfig: &tls.Config{
+			InsecureSkipVerify: config.SkipTLSVerification,
+		},
+	}
+
+	if config.TLSEnabled {
+		redisOpts.TLSConfig = &tls.Config{
+			InsecureSkipVerify: config.SkipTLSVerification,
+		}
+	}
+
+	redisCl = redis.NewClient(redisOpts)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+	redisCtx = ctx
+	
+	return nil
+}
+
+/*
+ * Common Methods
+ */
+func Init() {
+	config := configuration.ConfigHolder.DatabaseConfiguration
+
+	if config.EmbeddedRedisConfiguration.Enabled {
+		InitEmbeddedRedis()
+	}
+
+	if config.RedisConfiguration.Enabled {
+		InitRedis(config.RedisConfiguration)
+	}
+}
+
+func SetRedisValue(name string, arg any) error {
+	return redisCl.Set(redisCtx, name, arg, 0).Err()
+}
+
+func GetRedisValue(name string) (any, error) {
+	return redisCl.Get(redisCtx, name).Result()
+}
