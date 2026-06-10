85b462b79c043d7dfe076afc22344d5bfd028f34
BuffOvrFlw <ffrick0001980@proton.me>
2026-06-10 16:27:26 +0000

    comment out code for a full rewrite

--- internal/https/core/https_core.go
+++ internal/https/core/https_core.go
@@ -1,107 +1,107 @@
 package https_core
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/IzomSoftware/GinWrapper/internal/configuration"
-	"github.com/IzomSoftware/GinWrapper/internal/logger"
-	"github.com/IzomSoftware/GinWrapper/internal/responses"
+// 	"github.com/IzomSoftware/GinWrapper/internal/configuration"
+// 	"github.com/IzomSoftware/GinWrapper/internal/logger"
+// 	"github.com/IzomSoftware/GinWrapper/internal/responses"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
 
-// The HTTPS server struct. contains the gin.Engine which is the Router.
-type HttpsServer struct {
-	Router *gin.Engine
-}
+// // The HTTPS server struct. contains the gin.Engine which is the Router.
+// type HttpsServer struct {
+// 	Router *gin.Engine
+// }
 
-// The middleware, which gets called when every single connection begins.
-func middleware(context *gin.Context) {
-	logger.LogConnection(context)
+// // The middleware, which gets called when every single connection begins.
+// func middleware(context *gin.Context) {
+// 	logger.LogConnection(context)
 
-	connectionRequest := context.FullPath()
+// 	connectionRequest := context.FullPath()
 
-	// We can't just provide every single protection without any
-	// Storage configured.
-	if configuration.IsStorageConfigured() {
-		for _, req := range responses.Responses {
-			for _, address := range req.Addresses {
-				if connectionRequest == address {
-					req.OnProtected(context)
-				}
-			}
-		}
-	}
+// 	// We can't just provide every single protection without any
+// 	// Storage configured.
+// 	if configuration.IsStorageConfigured() {
+// 		for _, req := range responses.Responses {
+// 			for _, address := range req.Addresses {
+// 				if connectionRequest == address {
+// 					req.OnProtected(context)
+// 				}
+// 			}
+// 		}
+// 	}
 
-	context.Next()
-}
+// 	context.Next()
+// }
 
-// Registers every single path
-func (H *HttpsServer) RegisterPaths() {
-	for _, req := range responses.Responses {
-		for _, address := range req.Addresses {
-			H.Router.Handle(req.Type, address, req.Handler)
-		}
-	}
-}
+// // Registers every single path
+// func (H *HttpsServer) RegisterPaths() {
+// 	for _, req := range responses.Responses {
+// 		for _, address := range req.Addresses {
+// 			H.Router.Handle(req.Type, address, req.Handler)
+// 		}
+// 	}
+// }
 
-// Listens & serves the HTTP/HTTPS server
-func (H *HttpsServer) ListenAndServe(templatesDir string, assetsDir string) {
-	httpConfig := configuration.ConfigHolder.HTTPServer
+// // Listens & serves the HTTP/HTTPS server
+// func (H *HttpsServer) ListenAndServe(templatesDir string, assetsDir string) {
+// 	httpConfig := configuration.ConfigHolder.HTTPServer
 
-	if !httpConfig.Enabled {
-		return
-	}
+// 	if !httpConfig.Enabled {
+// 		return
+// 	}
 
-	gin.SetMode(gin.ReleaseMode)
+// 	gin.SetMode(gin.ReleaseMode)
 
-	H.Router = gin.New()
-	H.Router.Use(middleware)
-	H.Router.LoadHTMLGlob(templatesDir)
-	H.Router.Static(assetsDir, "."+assetsDir)
+// 	H.Router = gin.New()
+// 	H.Router.Use(middleware)
+// 	H.Router.LoadHTMLGlob(templatesDir)
+// 	H.Router.Static(assetsDir, "."+assetsDir)
 
-	// Register the 404 screen
-	H.Router.NoRoute(responses.NoRoute)
+// 	// Register the 404 screen
+// 	H.Router.NoRoute(responses.NoRoute)
 
-	isAnyUserPassAPIUsed := false
-	isAnyJWTAPIUsed := false
+// 	isAnyUserPassAPIUsed := false
+// 	isAnyJWTAPIUsed := false
 
-	for name, req := range responses.Responses {
-		for _, address := range req.Addresses {
-			if req.Protections.JWT {
-				isAnyUserPassAPIUsed = true
-				isAnyJWTAPIUsed = true
-			}
-			logger.Logger.Info(fmt.Sprintf("Registering Route -> %s - %s <-", name, address))
-		}
-	}
+// 	for name, req := range responses.Responses {
+// 		for _, address := range req.Addresses {
+// 			if req.Protections.JWT {
+// 				isAnyUserPassAPIUsed = true
+// 				isAnyJWTAPIUsed = true
+// 			}
+// 			logger.Logger.Info(fmt.Sprintf("Registering Route -> %s - %s <-", name, address))
+// 		}
+// 	}
 
-	// Register additional UserPass APIs
-	if isAnyUserPassAPIUsed || configuration.ConfigHolder.Protections.UserPassAPI {
-		responses.ActivateUserPassAPI()
-	}
+// 	// Register additional UserPass APIs
+// 	if isAnyUserPassAPIUsed || configuration.ConfigHolder.Protections.UserPassAPI {
+// 		responses.ActivateUserPassAPI()
+// 	}
 
-	// Register additional JWT APIs
-	if isAnyJWTAPIUsed {
-		responses.ActivateJWTAPI()
-	}
+// 	// Register additional JWT APIs
+// 	if isAnyJWTAPIUsed {
+// 		responses.ActivateJWTAPI()
+// 	}
 
-	// Finalize the path registeration (including APIs we provide)
-	H.RegisterPaths()
+// 	// Finalize the path registeration (including APIs we provide)
+// 	H.RegisterPaths()
 
-	addr := fmt.Sprintf("%s:%d", configuration.ConfigHolder.HTTPServer.Address, configuration.ConfigHolder.HTTPServer.Port)
+// 	addr := fmt.Sprintf("%s:%d", configuration.ConfigHolder.HTTPServer.Address, configuration.ConfigHolder.HTTPServer.Port)
 
-	logger.Logger.Info(fmt.Sprintf("Listening on %s", addr))
+// 	logger.Logger.Info(fmt.Sprintf("Listening on %s", addr))
 
-	var err error
+// 	var err error
 
-	if httpConfig.TlsConfiguration.Enable {
-		err = H.Router.RunTLS(addr, httpConfig.TlsConfiguration.CertFile, httpConfig.TlsConfiguration.KeyFile)
-	} else {
-		err = H.Router.Run(addr)
-	}
+// 	if httpConfig.TlsConfiguration.Enable {
+// 		err = H.Router.RunTLS(addr, httpConfig.TlsConfiguration.CertFile, httpConfig.TlsConfiguration.KeyFile)
+// 	} else {
+// 		err = H.Router.Run(addr)
+// 	}
 
-	if err != nil {
-		logger.Logger.Error(err)
-	}
-}
+// 	if err != nil {
+// 		logger.Logger.Error(err)
+// 	}
+// }
